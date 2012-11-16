d0eae61b82fab3e50e5f00cf2577f32fa8e7a084
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-11-16 16:58:16 +0100

    experimental

--- overrides.rb
+++ overrides.rb
@@ -11,3 +11,19 @@
 #
 # See config.yml to set the prefix:/opt/autoproj/2.0 globally for all packages.
 
+
+Autoproj.env_set 'TARGET', 'i386-rtems'
+Autoproj.env_set 'BSP', 'pc486'
+
+Autoproj.prefix = File.join("install",ENV['TARGET'])
+
+
+Autobuild::Package.each do |pkg_name, pkg|
+#Autoproj.manifest.each_package do |pkg|
+    pkg.builddir = "build-#{ENV['TARGET']}" if pkg.kind_of?(Autobuild::Configurable)
+end
+
+
+
+
+
